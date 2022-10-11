b3a4761ae3659c744430836a93c0f42f11e0fd6e
hungnd <ints.hungnd@gmail.com>
2022-10-11 17:17:31 +0700

    finsh-project

---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+      <div>
+            <div class="nav"></div>
+            <div class="container mx-auto">
+                  <h1 class=" my-3 w-96 display-in bg-slate-500">{{ title }}</h1>
+                  <div class="my-4 w-96 bg-orange-300">
+                        <select class="py-2" name="sort" id="sort">
+                              <option value="">Giá từ cao đến thấp</option>
+                              <option value="">Giá từ thấp đến cao</option>
+                        </select>
+                  </div>
+                  <hr>
+            </div>
+      </div>
+</template>
+<script>
+export default {
+      setup() {
+            const title = "Tất cả các sản phẩm";
+            const data = fetch('https://fakestoreapi.com/products')
+                  .then((response) => response.json())
+                  .then((data) => console.log(data));
+            // console.log(data.title(0) + "dbhcbdhbh");
+            return { title, data };
+      },
+};
+</script>
